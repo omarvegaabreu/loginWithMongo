681a67320313f5c965b3a64926e99d2053158c4f
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-02 22:09:28 -0400

    completed welcome, dashboard login register

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 //Login page
-router.get("login", (req, res) => res.render("login"));
+router.get("/login", (req, res) => res.render("login"));
 
 //Register page
 router.get("/register", (req, res) => res.render("register"));
